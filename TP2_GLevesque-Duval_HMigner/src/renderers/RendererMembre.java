4d9c8c22b1a51cd7b46fb78c5ac75d04e59a2f9e
Gabriel <gab9987@gmail.com>
2019-04-22 10:27:28 -0400

    Fenêtres non redimensionnable

--- TP2_GLevesque-Duval_HMigner/src/renderers/RendererMembre.java
+++ TP2_GLevesque-Duval_HMigner/src/renderers/RendererMembre.java
@@ -8,7 +8,8 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
 public class RendererMembre extends DefaultTableCellRenderer{
-	
+
+	private static final long serialVersionUID = 1L;
 	private ImageIcon imgMembre, imgRien;
 	
 	public RendererMembre() {
